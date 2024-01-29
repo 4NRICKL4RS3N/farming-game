8076acdeee4cbd05df385a1f8a2ea40755d567ce
L4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2024-01-29 14:41:30 +0300

    Initial commit

---
+++ src/main/java/com/main/farminggame/FarmingGameApplication.java
@@ -0,0 +1,13 @@
+package com.main.farminggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmingGameApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/main/farminggame/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hellooo %s!", name);
+    }
+}
